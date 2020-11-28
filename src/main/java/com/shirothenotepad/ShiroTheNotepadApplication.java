7f10c8bb3cf0c3d8c6636151b1ee6c8670940e62
神崎　そら <banyvwangsheng@163.com>
2020-11-28 18:35:57 +0800

    Initial commit

---
+++ src/main/java/com/shirothenotepad/ShiroTheNotepadApplication.java
@@ -0,0 +1,13 @@
+package com.shirothenotepad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShiroTheNotepadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShiroTheNotepadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shirothenotepad/ShiroTheNotepadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shirothenotepad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShiroTheNotepadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
