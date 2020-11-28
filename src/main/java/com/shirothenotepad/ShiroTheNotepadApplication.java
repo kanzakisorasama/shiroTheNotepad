80cd6344a0734034b8403d4e69c92e9ae095d5ab
神崎　そら <banyvwangsheng@163.com>
2020-11-28 18:49:22 +0800

    Create project First use Github and Git

--- src/main/java/com/shirothenotepad/ShiroTheNotepadApplication.java
+++ src/main/java/com/shirothenotepad/ShiroTheNotepadApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ShiroTheNotepadApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ShiroTheNotepadApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ShiroTheNotepadApplication.class, args);
+    }
 
 }
